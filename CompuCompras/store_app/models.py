f939a42e7301b429fe02999bf0138854ff2fd360
Isai J. Saransig <isai.saransig@epn.edu.ec>
2024-01-06 20:24:11 -0500

    add images field

--- CompuCompras/store_app/models.py
+++ CompuCompras/store_app/models.py
@@ -4,6 +4,7 @@ class Producto(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to="productos", null=True, blank=True)
 
     class Meta:
         verbose_name = 'Producto'

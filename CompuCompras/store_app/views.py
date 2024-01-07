32c9b4a63b05ec1ab5d291544517f6d8ab346481
Isai J. Saransig <isai.saransig@epn.edu.ec>
2024-01-06 22:07:53 -0500

    add function get_productos

--- CompuCompras/store_app/views.py
+++ CompuCompras/store_app/views.py
@@ -10,5 +10,9 @@ def ofertas(request):
 def nuevos(request):
     return render(request, 'nuevos.html')
 
+def get_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'index.html', {'productos': productos})
+
 def carrito(request):
     return render(request, 'carrito.html')

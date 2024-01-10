1e4c5107d7a668ed33c2ac6a59493b7df061588c
MikeEncalada <133013402+MikeEncalada@users.noreply.github.com>
2024-01-10 12:45:14 -0500

    Guardar el chat en la base de datos

--- CompuCompras/chat_app/views.py
+++ CompuCompras/chat_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Mensaje
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def chat(request):
-    return render(request, 'chat.html')
+    todos_los_mesajes = Mensaje.objects.all()
+
+    return render(request, 'chat.html', {'todos_los_mensajes': todos_los_mesajes})
